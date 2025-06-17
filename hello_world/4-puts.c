f3b8d6b0e7d3ccf06c8a8d9bbe2b5d2406893529
zaz9098 <zadeleh@hotmail.com>
2025-06-17 14:57:55 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * puts "Programming is like building a multilingual puzzle"
+ * puts "Programming is like building a multilingual puzzle
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
