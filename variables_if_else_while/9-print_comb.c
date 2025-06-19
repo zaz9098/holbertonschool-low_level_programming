b037bb96fd3d71864f1c30d0a2d884cb42ff6bb0
zaz9098 <zadeleh@hotmail.com>
2025-06-19 10:37:17 +0000

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,9 +12,13 @@ int main(void)
 	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+
 	putchar('\n');
 
 	return (0);
