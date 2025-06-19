0cd9ddd9a78ccfbb1a7f41bae85715c23a7498e5
zaz9098 <zadeleh@hotmail.com>
2025-06-19 12:43:25 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		putchar(word[i]);
+
+	putchar('\n');
+
+	return (0);
+}
