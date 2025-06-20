90b30f0f79bd29dcbcace71b96ea245df79fe84a
zaz9098 <zadeleh@hotmail.com>
2025-06-20 11:34:18 +0000

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
-* main - Entry point
-* Description : prints the word _putchar followed by a new line
+* Description : prints the alphabet
+*
 * Return: Always 0 (Success)
 */
 void print_alphabet(void)
-int main(void)
 {
-	char ch;
-	
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		_putchar(ch);
-	}
-	_putchar('\n');
-	
-	return (0);
+        char ch;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+                _putchar(ch);
+        }
+        _putchar('\n');
+
+        return (0);
 
 }
