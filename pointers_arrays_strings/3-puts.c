e57463cb2376cd5999db821cb711d92a6aba7709
zaz9098 <zadeleh@hotmail.com>
2025-06-26 16:39:51 +0000

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-  * _puts - Prints a string
-  * @str: The string to print
-  *
-  * Return: void
-  */
+*_puts - main entry
+*@str: string
+*
+*Write a function that prints a string, followed by a new line, to stdout.
+*/
 void _puts(char *str)
 {
 	while (*str)
