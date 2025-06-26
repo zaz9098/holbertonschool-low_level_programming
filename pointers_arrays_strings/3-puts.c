360321f3b1cf9a6563483c1e1da8cd9d9dde64b8
zaz9098 <zadeleh@hotmail.com>
2025-06-26 15:52:41 +0000

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -8,14 +8,10 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while (i >= 0)
+	while (str[i] != '\0')
 	{
-		if (str[i] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
 		_putchar(str[i]);
 		i++;
 	}
+	_putchar('\n');
 }
